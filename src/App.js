30eb32f70c1e3a57a9a30d962c871ada3930bfb1
Kaur Järvpõld <kaurjarvpold@gmail.com>
2020-11-24 20:17:25 +0200

    common styles file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import styles from 'src/styles/Styles.js';
 
 const App: () => React$Node = () => {
   return (
     <>
-      <View>
+      <View style={styles.container}>
         <Text>Hello World!</Text>
       </View>
     </>
