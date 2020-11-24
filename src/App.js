c5bfcb4d85f66c26e1e7b12594c5283f21899865
Kaur Järvpõld <kaurjarvpold@gmail.com>
2020-11-24 22:34:38 +0200

    add firebase phone authentication

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import styles from 'src/styles/Styles.js';
+import SignIn from 'src/screens/SignIn.js';
 
 const App: () => React$Node = () => {
   return (
-    <>
-      <View style={styles.container}>
-        <Text>Hello World!</Text>
-      </View>
-    </>
+    <View style={styles.container}>
+      <SignIn />
+    </View>
   );
 };
 
---
+++ src/screens/SignIn.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+import {View, StatusBar, Button, TextInput, Alert} from 'react-native';
+import auth from '@react-native-firebase/auth';
+
+function PhoneSignIn() {
+  const [confirm, setConfirm] = useState(null);
+  const [code, setCode] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  async function signInWithPhoneNumber() {
+    const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
+    setConfirm(confirmation);
+  }
+
+  async function confirmCode() {
+    try {
+      await confirm.confirm(code);
+    } catch (error) {
+      Alert.alert('Invalid code!');
+    }
+  }
+
+  if (!confirm) {
+    return (
+      <View>
+        <TextInput
+          value={phoneNumber}
+          onChangeText={(text) => setPhoneNumber(text)}
+        />
+        <Button
+          title="Sign In With Phone Number"
+          onPress={() => {
+            signInWithPhoneNumber();
+          }}
+        />
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <TextInput value={code} onChangeText={(text) => setCode(text)} />
+      <Button title="Confirm Code" onPress={() => confirmCode()} />
+    </View>
+  );
+}
+
+const SignIn: () => React$Node = () => {
+  return (
+    <View>
+      <PhoneSignIn />
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default SignIn;
