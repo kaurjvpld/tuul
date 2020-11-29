f8eb1de6022dd81bee15202734740faabdda7108
Kaur Järvpõld <kaurjarvpold@gmail.com>
2020-11-29 23:34:48 +0200

    roll back

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -12,12 +12,7 @@ import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
 
 const MapScreen: () => React$Node = () => {
-  const [mapCenter, setMapCenter] = useState({lat: 59.43699, lng: 24.753468});
-  const [userLocation, setUserLocation] = useState({
-    lat: 59.43699,
-    lng: 24.753468,
-  });
-  const [markers, setMarkers] = useState([]);
+  const [userLocation, setUserLocation] = useState(undefined);
 
   async function requestLocationPermission() {
     if (Platform.OS === 'android') {
@@ -27,83 +22,52 @@ const MapScreen: () => React$Node = () => {
     }
   }
 
+  function getUserLocation() {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        setUserLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      },
+      (error) => {
+        console.log(error.code, error.message);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 30000,
+        maximumAge: 10000,
+      },
+    );
+  }
+
   useEffect(() => {
     requestLocationPermission();
-
-    function updateMarkerIfPresent(markerToUpdate) {
-      let updateHappened = false;
-      let newMarkers = markers.map((marker) => {
-        if (marker.id === markerToUpdate.id) {
-          updateHappened = true;
-          return {...marker, position: markerToUpdate.position};
-        } else {
-          return marker;
-        }
-      });
-      setMarkers(newMarkers);
-      return updateHappened;
-    }
-
-    let userGeolocationUpdaterInterval = setInterval(
-      () =>
-        Geolocation.getCurrentPosition(
-          (position) => {
-            let userMarker = {
-              title: 'Your Location',
-              position: {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude,
-              },
-              url: require('src/assets/bluedot.png'),
-            };
-            if (!updateMarkerIfPresent(userMarker)) {
-              setMarkers([...markers, userMarker]);
-              // this.mapCenter = userMarker.position;
-            }
-            setUserLocation(userMarker.position);
-          },
-          (error) => {
-            console.log(error.code, error.message);
-          },
-          {
-            enableHighAccuracy: false,
-            timeout: 30000,
-            maximumAge: 10000,
-          },
-        ),
-      1000,
-    );
-
-    setMapCenter(userLocation);
-
-    return () => {
-      clearInterval(userGeolocationUpdaterInterval);
-    };
-  }, [markers, userLocation]);
+    getUserLocation();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        initialRegion={{
-          latitude: mapCenter.lat,
-          longitude: mapCenter.lng,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}>
-        {markers.map((marker, index) => (
+      {userLocation && (
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          initialRegion={{
+            latitude: userLocation.lat,
+            longitude: userLocation.lng,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}>
           <Marker
-            key={index}
             coordinate={{
-              latitude: marker.position.lat,
-              longitude: marker.position.lng,
+              latitude: userLocation.lat,
+              longitude: userLocation.lng,
             }}
             title={'Your Location'}>
-            <Image source={marker.url} />
+            <Image source={require('src/assets/bluedot.png')} />
           </Marker>
-        ))}
-      </MapView>
+        </MapView>
+      )}
       <Button title="Sign Out" onPress={() => auth().signOut()} />
     </View>
   );
