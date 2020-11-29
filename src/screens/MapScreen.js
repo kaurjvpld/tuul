1def35568d07aaa49d3631d83784f305b81fccb3
Kaur Järvpõld <kaurjarvpold@gmail.com>
2020-11-29 18:12:33 +0200

    display marker for user location

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,21 +1,73 @@
-import React from 'react';
-import {View, Button, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Button,
+  StyleSheet,
+  Platform,
+  PermissionsAndroid,
+} from 'react-native';
 import auth from '@react-native-firebase/auth';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
 
 const MapScreen: () => React$Node = () => {
+  const [mapCenter, setMapCenter] = useState({lat: 59.43699, lng: 24.753468});
+
+  async function requestLocationPermission() {
+    if (Platform.OS === 'android') {
+      await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      );
+    }
+  }
+
+  function getGeoLocation() {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        let _position = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        setMapCenter(_position);
+      },
+      (error) => {
+        // See error code charts below.
+        console.log(error.code, error.message);
+      },
+      {
+        enableHighAccuracy: false,
+        timeout: 30000,
+        maximumAge: 10000,
+      },
+    );
+  }
+
+  useEffect(() => {
+    requestLocationPermission();
+    getGeoLocation();
+  });
+
   return (
     <View style={styles.container}>
       <MapView
         provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
         style={styles.map}
         region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: mapCenter.lat,
+          longitude: mapCenter.lng,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
-        }}
-      />
+        }}>
+        <MapView.Marker
+          coordinate={{
+            latitude: mapCenter.lat,
+            longitude: mapCenter.lng,
+          }}
+          title={'Your Location'}
+          draggable
+        />
+      </MapView>
       <Button title="Sign Out" onPress={() => auth().signOut()} />
     </View>
   );
